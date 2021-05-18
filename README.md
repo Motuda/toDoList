58da3e4d21ebfc929132325d64a07ce34070874d
Pato Díaz <patdiazar@gmail.com>
2021-05-18 16:24:56 -0300

    Comienzo a agregar firebase

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+fire
+firebase login
+firebase init
+firebase deploy
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import NewTaskFields from "../src/NewTaskFields/NewTaskFields";
 import './App.css';
 import TaskList from "./TaskList/TaskList";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBbQPqQ1xPaSlg2SYQXIl9CZE3bNPKyEnI",
+  authDomain: "todolist-motuda.firebaseapp.com",
+  projectId: "todolist-motuda",
+  storageBucket: "todolist-motuda.appspot.com",
+  messagingSenderId: "572602396151",
+  appId: "1:572602396151:web:4ccc2485127281917952f2"
+};
+
 function App() {
   return (
     <div className="App">
